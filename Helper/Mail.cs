b63001e0090237e2b7b1dd83e8bc24fbdac194ba
shashank kishan <sk1994kishan@gmail.com>
2017-09-10 20:12:45 +0530

    Add files via upload

---
+++ Helper/Mail.cs
@@ -0,0 +1,24 @@
+﻿using OnlineBanking.Data;
+using System;
+using System.Net.Mail;
+
+namespace OnlineBanking.Helpers
+{
+    public class Mail
+    { 
+            public void SendEmail(MailModel m)
+             { 
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress("chinthareddysaiteja@gmail.com");
+                mail.To.Add(m.toAddress);
+                mail.Subject = m.subject;
+                mail.Body = m.body;
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("chinthareddysaiteja@gmail.com", "nani1324");
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+
+            }
+        }
+ }
---
+++ Helper/Password.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace OnlineBanking.Helpers
+{
+    public class Password
+    {
+        public string GeneratePassword()
+        {
+            const string validChars = "abcdefghijklmnopqrstuvwxyz";
+            const string validChars1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string validChars2 = "1234567890";
+            const string validChars3 = "#?!@$%^&*-";
+
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            int length = 8;
+
+            res.Append(validChars[rnd.Next(validChars.Length)]);
+            res.Append(validChars1[rnd.Next(validChars1.Length)]);
+            res.Append(validChars3[rnd.Next(validChars3.Length)]);
+            while (3 < length--)
+            {
+                res.Append(validChars2[rnd.Next(validChars2.Length)]);
+            }
+
+            return res.ToString();
+        }
+    }
+}
